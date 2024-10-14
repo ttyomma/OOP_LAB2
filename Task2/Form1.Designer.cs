eb4135d09160934046ef3b058ddb71c31037228b
Artem <artemkolisnychenko@gmail.com>
2024-10-14 16:34:28 +0300

    Add files via upload

---
+++ Task2/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Lab2._2_Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GridStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.ColorButton = new System.Windows.Forms.ToolStripButton();
+            this.SizeButton = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusSizeLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.changeSizeStrip = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1190, 631);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1190, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Export";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.GridStrip});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // GridStrip
+            // 
+            this.GridStrip.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeSizeStrip});
+            this.GridStrip.Name = "GridStrip";
+            this.GridStrip.Size = new System.Drawing.Size(180, 22);
+            this.GridStrip.Text = "Grid";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.ColorButton,
+            this.SizeButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1190, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ColorButton.Image = ((System.Drawing.Image)(resources.GetObject("ColorButton.Image")));
+            this.ColorButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.ColorButton.Size = new System.Drawing.Size(50, 22);
+            this.ColorButton.Text = "Color";
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // SizeButton
+            // 
+            this.SizeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.SizeButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SizeButton.Name = "SizeButton";
+            this.SizeButton.Size = new System.Drawing.Size(31, 22);
+            this.SizeButton.Tag = "";
+            this.SizeButton.Text = "Size";
+            this.SizeButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.SizeButton.Click += new System.EventHandler(this.SizeButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.StatusSizeLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 633);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1190, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // StatusSizeLabel
+            // 
+            this.StatusSizeLabel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.StatusSizeLabel.Name = "StatusSizeLabel";
+            this.StatusSizeLabel.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.StatusSizeLabel.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StatusSizeLabel.Size = new System.Drawing.Size(88, 17);
+            this.StatusSizeLabel.Text = "Scale: 100%";
+            this.StatusSizeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // changeSizeStrip
+            // 
+            this.changeSizeStrip.Name = "changeSizeStrip";
+            this.changeSizeStrip.Size = new System.Drawing.Size(180, 22);
+            this.changeSizeStrip.Text = "Change Size...";
+            this.changeSizeStrip.Click += new System.EventHandler(this.changeSizeStrip_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 655);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem GridStrip;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripStatusLabel StatusSizeLabel;
+        private System.Windows.Forms.ToolStripButton SizeButton;
+        private System.Windows.Forms.ToolStripButton ColorButton;
+        private System.Windows.Forms.ToolStripMenuItem changeSizeStrip;
+    }
+}
+
---
+++ Task2/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab2._2_Paint
+{
+    public partial class Form1 : Form
+    {
+        private Color penColor = Color.Black;
+        private bool isDrawing = false;
+        private float zoomFactor = 1.0f;
+        private Bitmap originalImage;
+        private Bitmap scaledImage;
+        private Point previousPoint;
+        private int penWidth = 1;
+        private bool isPanning = false;
+        private Point panStartPoint;
+        private Point imageOffset = new Point(0, 0);
+
+        private int gridStep = 10; // основне завдання - крок сітки
+
+        public Form1()
+        {
+            InitializeComponent();
+            originalImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            scaledImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = scaledImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            UpdateScaledImage();
+        }
+
+        private void DrawGrid(Graphics g) // основне завдання - метод малювання сітки
+        {
+            for (int x = 0; x < originalImage.Width; x += gridStep)
+            {
+                g.DrawLine(Pens.LightGray, x, 0, x, originalImage.Height);
+            }
+
+            for (int y = 0; y < originalImage.Height; y += gridStep)
+            {
+                g.DrawLine(Pens.LightGray, 0, y, originalImage.Width, y);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle)
+            {
+                isPanning = true;
+                panStartPoint = e.Location;
+            }
+            else
+            {
+                isDrawing = true;
+
+                int centerX = (pictureBox1.Width - (int)(originalImage.Width * zoomFactor)) / 2;
+                int centerY = (pictureBox1.Height - (int)(originalImage.Height * zoomFactor)) / 2;
+
+                int x = (int)((e.X - centerX) / zoomFactor);
+                int y = (int)((e.Y - centerY) / zoomFactor);
+
+                previousPoint = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle)
+            {
+                isPanning = false;
+            }
+            else
+            {
+                isDrawing = false;
+                previousPoint = Point.Empty;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolStripStatusLabel1.Text = $"{e.X}, {e.Y - 25} пкс";
+
+            if (isPanning)
+            {
+                int offsetX = e.X - panStartPoint.X;
+                int offsetY = e.Y - panStartPoint.Y;
+                imageOffset.X += offsetX;
+                imageOffset.Y += offsetY;
+
+                UpdateScaledImage();
+                panStartPoint = e.Location;
+            }
+            else
+            {
+                if (isDrawing)
+                {
+                    int centerX = (pictureBox1.Width - (int)(originalImage.Width * zoomFactor)) / 2 + imageOffset.X;
+                    int centerY = (pictureBox1.Height - (int)(originalImage.Height * zoomFactor)) / 2 + imageOffset.Y;
+
+                    int x = (int)((e.X - centerX) / zoomFactor);
+                    int y = (int)((e.Y - centerY) / zoomFactor);
+                    int prevX = (int)((previousPoint.X - centerX) / zoomFactor);
+                    int prevY = (int)((previousPoint.Y - centerY) / zoomFactor);
+
+                    if (previousPoint != Point.Empty)
+                    {
+                        using (Graphics bufferGraphics = Graphics.FromImage(originalImage))
+                        {
+                            DrawLine(bufferGraphics, prevX, prevY, x, y, penColor);
+                        }
+                        UpdateScaledImage();
+                    }
+
+                    previousPoint = e.Location;
+                }
+            }
+        }
+
+        // метод для апскейлу на майбутнє
+        private void UpdateScaledImage()
+        {
+            using (Graphics g = Graphics.FromImage(scaledImage))
+            {
+                g.Clear(pictureBox1.BackColor);
+            }
+
+            scaledImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(scaledImage))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+
+                DrawGrid(g);
+
+                int x = (pictureBox1.Width - (int)(originalImage.Width * zoomFactor)) / 2 + imageOffset.X;
+                int y = (pictureBox1.Height - (int)(originalImage.Height * zoomFactor)) / 2 + imageOffset.Y;
+                g.DrawImage(originalImage, x, y, (int)(originalImage.Width * zoomFactor), (int)(originalImage.Height * zoomFactor));
+
+                g.DrawRectangle(Pens.Black, x, y, (int)(originalImage.Width * zoomFactor) - 1, (int)(originalImage.Height * zoomFactor) - 1); // рамка
+            }
+
+            pictureBox1.Image = scaledImage;
+            pictureBox1.Invalidate();
+            pictureBox1.Update();
+        }
+
+        //алгоритму Брезенхема з інтернету для того, аби не було розриву між пікселями
+        private void DrawLine(Graphics g, int x1, int y1, int x2, int y2, Color color)
+        {
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
+            int sx = x1 < x2 ? 1 : -1;
+            int sy = y1 < y2 ? 1 : -1;
+            int err = (dx > dy ? dx : -dy) / 2;
+
+            while (true)
+            {
+                if (x1 >= 0 && x1 < originalImage.Width && y1 >= 0 && y1 < originalImage.Height)
+                {
+                    for (int i = -penWidth / 2; i <= penWidth / 2; i++)
+                    {
+                        for (int j = -penWidth / 2; j <= penWidth / 2; j++)
+                        {
+                            if (x1 + i >= 0 && x1 + i < originalImage.Width && y1 + j >= 0 && y1 + j < originalImage.Height)
+                            {
+                                originalImage.SetPixel(x1 + i, y1 + j, color);
+                            }
+                        }
+                    }
+                }
+
+                if (x1 == x2 && y1 == y2) break;
+
+                int e2 = err;
+
+                if (e2 > -dx)
+                {
+                    err -= dy;
+                    x1 += sx;
+                }
+
+                if (e2 < dy)
+                {
+                    err += dx;
+                    y1 += sy;
+                }
+            }
+        }
+
+
+
+        private void UpdatePictureBoxSize()
+        {
+            int oldWidth = pictureBox1.Width;
+            int oldHeight = pictureBox1.Height;
+
+            pictureBox1.Width = (int)(originalImage.Width * zoomFactor);
+            pictureBox1.Height = (int)(originalImage.Height * zoomFactor);
+
+            Bitmap newOriginalImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            using (Graphics g = Graphics.FromImage(newOriginalImage))
+            {
+                g.DrawImage(originalImage, 0, 0, oldWidth, oldHeight);
+            }
+            originalImage = newOriginalImage;
+            UpdateScaledImage();
+            UpdateStatusBar();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            zoomFactor += 0.1f;
+            UpdatePictureBoxSize();
+            UpdateStatusBar();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            zoomFactor -= 0.1f;
+            if (zoomFactor < 0.1f) zoomFactor = 0.1f;
+            UpdatePictureBoxSize();
+            UpdateStatusBar();
+        }
+
+        private void UpdateStatusBar()
+        {
+            string zoomText = $"Scale: {(int)(zoomFactor * 100)}%";
+            StatusSizeLabel.Text = zoomText;
+        }
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                penColor = colorDialog1.Color;
+            }
+        }
+
+        private void SizeButton_Click(object sender, EventArgs e)
+        {
+            using (NumericUpDown numericUpDown = new NumericUpDown())
+            {
+                numericUpDown.Minimum = 1;
+                numericUpDown.Maximum = 10;
+                numericUpDown.Value = penWidth;
+
+                Button okButton = new Button();
+                okButton.Text = "OK";
+                okButton.DialogResult = DialogResult.OK;
+
+                Form form = new Form();
+                form.Text = "Choose Size";
+                form.Controls.Add(numericUpDown);
+                form.Controls.Add(okButton);
+
+                numericUpDown.Location = new Point(10, 10);
+                okButton.Location = new Point(10, 40);
+                form.Size = new Size(230, 130);
+
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    penWidth = (int)numericUpDown.Value;
+                }
+            }
+        }
+
+        private void changeSizeStrip_Click(object sender, EventArgs e) // основне завдання - зміна кроку сітки в діалоговому вікні
+        {
+            using (NumericUpDown numericUpDown = new NumericUpDown())
+            {
+                numericUpDown.Minimum = 1;
+                numericUpDown.Maximum = 100;
+                numericUpDown.Value = gridStep;
+
+                Button okButton = new Button();
+                okButton.Text = "OK";
+                okButton.DialogResult = DialogResult.OK;
+
+                Form form = new Form();
+                form.Text = "Change Grid";
+                form.Controls.Add(numericUpDown);
+                form.Controls.Add(okButton);
+
+                numericUpDown.Location = new Point(10, 10);
+                okButton.Location = new Point(10, 40);
+
+                form.Size = new Size(280, 120);
+
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    gridStep = (int)numericUpDown.Value;
+                    UpdateScaledImage();
+                }
+            }
+        }
+    }
+}
